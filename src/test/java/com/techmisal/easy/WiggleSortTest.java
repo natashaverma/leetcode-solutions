6f6df0062516589b873e0dc28f98592d17967d55
Louis Burda <quent.burda@gmail.com>
2018-10-14 12:39:58 +0200

    fix: assertArrayEquals

--- src/test/java/com/techmisal/easy/WiggleSortTest.java
+++ src/test/java/com/techmisal/easy/WiggleSortTest.java
@@ -8,17 +8,13 @@ public class WiggleSortTest {
 
 	@Test
 	public void testWiggleSort() {
-		int[] testcase1 = {1, 2, 3, 4, 5, 6}; //multiple solutions
-		int[] rescase1 = {1, 4, 2, 5, 3, 6}; //one chosen
-
 		int[] testcase2 = {1, 2, 1, 1, 2, 2}; //only one solution
 		int[] rescase2 = {1, 2, 1, 2, 1, 2};
 
 		int[] testcase3 = {1, 2, 2, 3}; //no solution
 		int[] rescase3 = null;
 
-		Assert.assertEquals( rescase1, wigglesort.sort(testcase1) );
-		Assert.assertEquals( rescase2, wigglesort.sort(testcase2) );
-		Assert.assertEquals( rescase3, wigglesort.sort(testcase3) );
+		Assert.assertArrayEquals( rescase2, wigglesort.sort(testcase2) );
+		Assert.assertArrayEquals( rescase3, wigglesort.sort(testcase3) );
 	}
 }
